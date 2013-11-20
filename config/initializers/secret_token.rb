b026bc603254f0545187fd238565209181f21640
zrivest <zrivest@gmail.com>
2013-11-20 17:13:40 -0600

    first commit with twitter bootstrap template added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GillesAviationSite3::Application.config.secret_token = 'c8ddadd1b838c166b7f89124f3902cf6b2abd04e1b0cb1172b7de85554b3dba699c815aacac4390bff9cee70a85ebcbd3a87d4e8c1de6bc76de604eeaa785dc2'
